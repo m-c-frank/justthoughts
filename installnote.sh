459ea6e76f84be5d41e7ba8ad88bffcd305a9126
mcfrank <mcfrank@goldenticket.local>
2023-11-21 07:41:00 +0100

    improved installnote

--- installnote.sh
+++ installnote.sh
@@ -3,12 +3,15 @@
 # Usage check and directory existence check
 [ "$#" -ne 1 ] || [ ! -d "$1" ] && echo "Usage: $0 path/to/directory" && exit 1
 
-# Updating REPO_DIR in createnote.sh
-sed -i "s|REPO_DIR=.*|REPO_DIR=$1|" createnote.sh
+# Create a temporary copy of createnote.sh
+temp_file=$(mktemp)
+cp createnote.sh "$temp_file"
 
-# Adding createnote.sh to the user's path
-chmod +x createnote.sh
-cp "$(pwd)/createnote.sh" "$HOME/.local/bin/createnote"
+# Updating REPO_DIR in the temporary file
+sed -i "s|REPO_DIR=.*|REPO_DIR=$1|" "$temp_file"
+
+# Move the temporary file to the user's path
+chmod +x "$temp_file"
+mv "$temp_file" "$HOME/.local/bin/createnote"
 
 echo "Setup complete. Use 'createnote' to create a new note."
-
