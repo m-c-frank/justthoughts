3ae1428c9c23400be7452d49002f9ef94adb04c9
mcfrank <mcfrank@goldenticket.local>
2023-11-25 07:51:05 +0100

    updated llmjob myself

--- llmjob.sh
+++ llmjob.sh
@@ -3,21 +3,10 @@ ISSUE_NUMBER=$1
 REPO_NAME=$(basename $(git remote get-url origin) .git)
 REPO_OWNER=$(basename $(dirname $(git remote get-url origin)) | sed 's:.*/::')
 
-# Validate Variables
-if [ -z "$REPO_NAME" ] || [ -z "$REPO_OWNER" ]; then
-    echo "Error: Repository name or owner not found."
-    exit 1
-fi
+echo "$GH_PAT" | gh auth login --with-token
 
 # GitHub API URL
-ISSUE_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}"
+curl "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}" > issue_details.json
 
-# Fetch Issue Data
-curl -H "Authorization: token $GH_TOKEN" "$ISSUE_URL" > issue_data.json
-
-# Validate Issue Data
-if [ ! -s issue_data.json ]; then
-    echo "Error: Failed to fetch issue data or issue data is empty."
-    exit 1
-fi
+python scripts/llm_issue_solver.py issue_data.json
 
