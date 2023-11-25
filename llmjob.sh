cf4b395af9b9d0c79e75cf4b2f3679a117cc2695
mcfrank <mcfrank@goldenticket.local>
2023-11-25 07:58:17 +0100

    logging for sanity

--- llmjob.sh
+++ llmjob.sh
@@ -5,6 +5,11 @@ ISSUE_NUMBER=$1
 REPO_NAME=$(basename $(git remote get-url origin) .git)
 REPO_OWNER=$(basename $(dirname $(git remote get-url origin)))
 
+echo "$REPO_NAME"
+echo "$REPO_OWNER"
+echo "$ISSUE_NUMBER"
+echo "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}" > issue_details.json
+
 # Authenticate with GitHub CLI using GH_PAT
 echo "$GH_PAT" | gh auth login --with-token
 
