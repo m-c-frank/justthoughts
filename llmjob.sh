64f79969b9fab6a24608cfc4318792e9f91671c7
mcfrank <mcfrank@goldenticket.local>
2023-11-25 07:45:23 +0100

    trying chatgpt fix

--- llmjob.sh
+++ llmjob.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
-
 # Variables
 ISSUE_NUMBER=$1
 REPO_NAME=$(basename $(git remote get-url origin) .git)
-REPO_OWNER=$(basename $(dirname $(git remote get-url origin)))
+REPO_OWNER=$(basename $(dirname $(git remote get-url origin)) | sed 's:.*/::')
 
-# Authenticate with GitHub CLI
-echo "$GH_TOKEN" | gh auth login --with-token
+# Validate Variables
+if [ -z "$REPO_NAME" ] || [ -z "$REPO_OWNER" ]; then
+    echo "Error: Repository name or owner not found."
+    exit 1
+fi
+
+# GitHub API URL
+ISSUE_URL="https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}"
 
 # Fetch Issue Data
-curl -H "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}" \
-     > issue_data.json
+curl -H "Authorization: token $GH_TOKEN" "$ISSUE_URL" > issue_data.json
 
-# Execute Python Script for LLM Processing
-python scripts/llmjob.py issue_data.json
+# Validate Issue Data
+if [ ! -s issue_data.json ]; then
+    echo "Error: Failed to fetch issue data or issue data is empty."
+    exit 1
+fi
 
