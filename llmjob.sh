ab6006efd4030b7a070215e923e1a5430b949ce0
mcfrank <mcfrank@goldenticket.local>
2023-11-25 07:54:55 +0100

    using the issueblogger way to determine the url stuff

--- llmjob.sh
+++ llmjob.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 # Variables
 ISSUE_NUMBER=$1
 REPO_NAME=$(basename $(git remote get-url origin) .git)
-REPO_OWNER=$(basename $(dirname $(git remote get-url origin)) | sed 's:.*/::')
+REPO_OWNER=$(basename $(dirname $(git remote get-url origin)))
 
+# Authenticate with GitHub CLI using GH_PAT
 echo "$GH_PAT" | gh auth login --with-token
 
-# GitHub API URL
+# Fetch Issue Data
 curl "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${ISSUE_NUMBER}" > issue_details.json
 
 python scripts/llm_issue_solver.py issue_data.json
